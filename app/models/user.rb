79a6110b2b9ca06a61c4e7eeedc20c0e79b92dac
Momoji817 <momoko.kudo0817@gmail.com>
2022-04-08 08:27:11 +0900

    Add: authenticationモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_one :authentication, dependent: :destroy
+  accepts_nested_attributes_for :authentication
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :profile_image_url, presence: true
   validates :account_url, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_07_001719) do
+ActiveRecord::Schema.define(version: 2022_04_07_111631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.string "oauth_token", null: false
+    t.string "oauth_token_secret", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["provider", "uid", "user_id"], name: "index_authentications_on_provider_and_uid_and_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
