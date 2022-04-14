4713bbacf01a8e4bef24ad1c88fa4363b82b920e
Momoji817 <momoko.kudo0817@gmail.com>
2022-04-14 12:47:34 +0900

    Add: OAuth設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_pages#top'
+  
+  post 'oauth/callback', to: 'oauths#callback'
+  get 'oauth/callback', to: 'oauths#callback'
+  get 'oauth/:provider', to: 'oauths#oauth', as: 'auth_at_provider'
 end
