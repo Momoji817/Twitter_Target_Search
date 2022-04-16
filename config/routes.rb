22ca4e0bec4b00b4c0e9f6f26b04206dde6de782
Momoji817 <momoko.kudo0817@gmail.com>
2022-04-16 11:10:12 +0900

    Add: UserSessionsControllerの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post 'oauth/callback', to: 'oauths#callback'
   get 'oauth/callback', to: 'oauths#callback'
   get 'oauth/:provider', to: 'oauths#oauth', as: 'auth_at_provider'
+  delete 'logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[show destroy]
 end
