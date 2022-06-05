f3956b8dca1de3cf70b619e295ac9af6f12ecb0c
Momoko Kudo <88662582+Momoji817@users.noreply.github.com>
2022-06-06 08:01:16 +0900

    Add: 検索条件の設定 (#54)

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,4 @@ Rails.application.routes.draw do
   resources :users, only: %i[show destroy]
 
   get 'following_candidates', to: 'following_candidates#index'
-  post 'following_candidates', to: 'following_candidates#call'
 end
