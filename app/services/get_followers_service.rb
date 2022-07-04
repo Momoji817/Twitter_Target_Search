ec15c5796452dae7c0bbc5bebd417c4ac1ee8dc4
Momoji817 <momoko.kudo0817@gmail.com>
2022-07-04 21:25:37 +0900

    Fix: 画面表記の修正

--- app/services/get_followers_service.rb
+++ app/services/get_followers_service.rb
@@ -21,7 +21,7 @@ class GetFollowersService
       screen_name: "#{@screen_name}",
       skip_status: "true",
       include_user_entities: "false",
-      count: 100
+      count: 200
     }
     if @next_cursor.blank?
       @create_optional_params
