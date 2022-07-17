ed263208796a3460b304e4882d7cca5c3ceb70bd
Momoko Kudo <88662582+Momoji817@users.noreply.github.com>
2022-07-17 20:08:04 +0900

    Add: モデルスペックの設定 (#102)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    before { build(:user) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_most(50) }
+    it { is_expected.to validate_presence_of(:profile_image_url) }
+  end
+
+  describe 'associations' do
+    it { should have_one(:authentication).dependent(:destroy) }
+  end
 end
